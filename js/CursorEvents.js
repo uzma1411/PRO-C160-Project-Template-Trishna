01c1cc914619e5b4e9d95ea7b1d1e16caf5dbfb7
uzma1411 <47413610+uzma1411@users.noreply.github.com>
2023-04-16 07:24:29 +0530

    Update CursorEvents.js

--- js/CursorEvents.js
+++ js/CursorEvents.js
@@ -27,26 +27,10 @@ AFRAME.registerComponent("cursor-listener", {
   },
   handleMouseEnterEvents: function() {
     // Mouse Center Events
-    this.el.addEventListener("mouseenter", () => {
-      const placeContainer = document.querySelector("#places-container");
-      const { state } = placeContainer.getAttribute("tour");
-      if (state === "places-list") {
-        this.handlePlacesListState();
-      }
-    });
+    
   },
   handlePlacesListState: function() {
-    const id = this.el.getAttribute("id");
-    const placesId = ["place-home", "place-garden", "place-main_gate"];
-    if (placesId.includes(id)) {
-      const placeContainer = document.querySelector("#places-container");
-      placeContainer.setAttribute("cursor-listener", {
-        selectedItemId: id
-      });
-      this.el.setAttribute("material", {
-        opacity: 1
-      });
-    }
+   
   },
   handleMouseLeaveEvents: function() {
     // Mouse Leave Events
